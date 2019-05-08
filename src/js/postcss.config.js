082060cffe5350e31d7c52b1382e7dc024a000ca
Sorokin-Oleg <sorokin.world@gmail.com>
2019-05-08 18:28:04 +0300

    - Setting up the environment.

---
+++ src/js/postcss.config.js
@@ -0,0 +1,16 @@
+// npm install postcss-loader autoprefixer css-mqpacker cssnano --save-dev
+module.exports = {
+    plugins: [
+      require('autoprefixer'),
+      require('css-mqpacker'),
+      require('cssnano')({
+        preset: [
+          'default', {
+            discardComments: {
+              removeAll: true,
+            }
+          }
+        ]
+      })
+    ]
+  }
