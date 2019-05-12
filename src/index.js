4d60fdf65d3a1098266c92cf5a2157218169e2f9
Sorokin-Oleg <sorokin.world@gmail.com>
2019-05-12 16:42:05 +0300

    - Added favicon.

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App'
 import './main.scss'
-
-// ========================================
   
 ReactDOM.render(
     <App />,
