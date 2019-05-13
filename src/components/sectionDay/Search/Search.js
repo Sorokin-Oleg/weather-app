033371965643ffbd856eb9cab997b2d378d0317d
Sorokin-Oleg <sorokin.world@gmail.com>
2019-05-13 16:07:36 +0300

    - Create template component 'Search'; - Cyrillic support added.

--- src/components/sectionDay/Search/Search.js
+++ src/components/sectionDay/Search/Search.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
 import './Search.scss';
 
-
 const Search = (props) => {
     return(
-        <div className='container-search'></div>
+        <div className='container-search'>
+            <input type="text" placeholder='Enter the name of the city'/>
+        </div>
     );
 };
 
