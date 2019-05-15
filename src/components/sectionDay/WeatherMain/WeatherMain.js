5f6e3c6a3513d354b9aad47a7735f97e0db905c1
Sorokin-Oleg <sorokin.world@gmail.com>
2019-05-15 11:16:20 +0300

    - Creating template fo component (WeatherMain).

--- src/components/sectionDay/WeatherMain/WeatherMain.js
+++ src/components/sectionDay/WeatherMain/WeatherMain.js
@@ -3,7 +3,24 @@ import './WeatherMain.scss';
 
 const WeatherMain = (props) => {
     return ( 
-        <div className='container-weather-main'></div>
+        <div className='container-weather-main'>
+            <div>
+                <span>Visibility</span>
+                <span>4 km</span>
+            </div>
+            <div>
+                <span>Wind</span>
+                <span>2 m/s</span>
+            </div>
+            <div>
+                <span>Hymidity</span>
+                <span>75%</span>
+            </div>
+            <div>
+                <span>Pressure</span>
+                <span>710.00 mm</span>
+            </div>
+        </div>
     );
 };
 
